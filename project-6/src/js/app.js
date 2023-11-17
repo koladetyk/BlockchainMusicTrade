77c12b6b3fe15ec1a3a0fc482fbf5e82c6b7425a
ayokolade.erinle <47436776+koladetyk@users.noreply.github.com>
2023-11-17 08:04:33 +0100

    commit for app

--- project-6/src/js/app.js
+++ project-6/src/js/app.js
@@ -6,16 +6,13 @@ App = {
     upc: 0,
     metamaskAccountID: "0x0000000000000000000000000000000000000000",
     ownerID: "0x0000000000000000000000000000000000000000",
-    originFarmerID: "0x0000000000000000000000000000000000000000",
-    originFarmName: null,
-    originFarmInformation: null,
-    originFarmLatitude: null,
-    originFarmLongitude: null,
-    productNotes: null,
-    productPrice: 0,
+    artistID: "0x0000000000000000000000000000000000000000",
+    musicPrice: 0,
     distributorID: "0x0000000000000000000000000000000000000000",
-    retailerID: "0x0000000000000000000000000000000000000000",
+    recordLabelID: "0x0000000000000000000000000000000000000000",
     consumerID: "0x0000000000000000000000000000000000000000",
+    musicID: 0,
+    musicNotes: null,
 
     init: async function () {
         App.readForm();
@@ -27,31 +24,27 @@ App = {
         App.sku = $("#sku").val();
         App.upc = $("#upc").val();
         App.ownerID = $("#ownerID").val();
-        App.originFarmerID = $("#originFarmerID").val();
-        App.originFarmName = $("#originFarmName").val();
-        App.originFarmInformation = $("#originFarmInformation").val();
-        App.originFarmLatitude = $("#originFarmLatitude").val();
-        App.originFarmLongitude = $("#originFarmLongitude").val();
-        App.productNotes = $("#productNotes").val();
-        App.productPrice = $("#productPrice").val();
+        App.artistID = $("#artistID").val();
+        App.musicPrice = $("#musicPrice").val();
+        App.musicState = $("#musicState").val();
         App.distributorID = $("#distributorID").val();
-        App.retailerID = $("#retailerID").val();
+        App.recordLabelID = $("#recordLabelID").val();
         App.consumerID = $("#consumerID").val();
+        App.musicID = $("#musicID").val();
+        App.musicNotes = $("#musicNotes").val();
 
         console.log(
             App.sku,
             App.upc,
             App.ownerID, 
-            App.originFarmerID, 
-            App.originFarmName, 
-            App.originFarmInformation, 
-            App.originFarmLatitude, 
-            App.originFarmLongitude, 
-            App.productNotes, 
-            App.productPrice, 
+            App.artistID, 
+            App.musicPrice, 
+            App.musicState, 
             App.distributorID, 
-            App.retailerID, 
-            App.consumerID
+            App.recordLabelID, 
+            App.consumerID,
+            App.musicID,
+            App.musicNotes
         );
     },
 
@@ -108,8 +101,9 @@ App = {
             App.contracts.SupplyChain = TruffleContract(SupplyChainArtifact);
             App.contracts.SupplyChain.setProvider(App.web3Provider);
             
-            App.fetchItemBufferOne();
-            App.fetchItemBufferTwo();
+            App.fetchmusicBufferOnePartOne();
+            App.fetchmusicBufferOnePartTwo();
+            App.fetchmusicBufferOnePartThree();
             App.fetchEvents();
 
         });
@@ -131,186 +125,187 @@ App = {
 
         switch(processId) {
             case 1:
-                return await App.harvestItem(event);
+                return await App.makeMusic(event);
                 break;
             case 2:
-                return await App.processItem(event);
+                return await App.registerMusic(event);
                 break;
             case 3:
-                return await App.packItem(event);
+                return await App.verifyMusic(event);
                 break;
             case 4:
-                return await App.sellItem(event);
+                return await App.promoteMusic(event);
                 break;
             case 5:
-                return await App.buyItem(event);
+                return await App.distributeMusic(event);
                 break;
             case 6:
-                return await App.shipItem(event);
+                return await App.sellMusic(event);
                 break;
             case 7:
-                return await App.receiveItem(event);
+                return await App.buyMusic(event);
                 break;
             case 8:
-                return await App.purchaseItem(event);
+                return await App.listenToMusic(event);
                 break;
             case 9:
-                return await App.fetchItemBufferOne(event);
+                return await App.fetchmusicBufferOnePartOne(event);
                 break;
             case 10:
-                return await App.fetchItemBufferTwo(event);
+                return await App.fetchmusicBufferOnePartTwo(event);
+                break;
+            case 11:
+                return await App.fetchmusicBufferOnePartThree(event);
                 break;
             }
     },
 
-    harvestItem: function(event) {
+    makeMusic: function(event) {
         event.preventDefault();
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
-            return instance.harvestItem(
+            return instance.makeMusic(
+                App.sku,
                 App.upc, 
                 App.metamaskAccountID, 
-                App.originFarmName, 
-                App.originFarmInformation, 
-                App.originFarmLatitude, 
-                App.originFarmLongitude, 
-                App.productNotes
+                App.musicID, 
+                App.musicNotes
             );
         }).then(function(result) {
             $("#ftc-item").text(result);
-            console.log('harvestItem',result);
+            console.log('makeMusic',result);
         }).catch(function(err) {
             console.log(err.message);
         });
     },
 
-    processItem: function (event) {
+    registerMusic: function (event) {
         event.preventDefault();
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
-            return instance.processItem(App.upc, {from: App.metamaskAccountID});
+            return instance.registerMusic(App.upc, {from: App.metamaskAccountID});
         }).then(function(result) {
             $("#ftc-item").text(result);
-            console.log('processItem',result);
+            console.log('registerMusic',result);
         }).catch(function(err) {
             console.log(err.message);
         });
     },
     
-    packItem: function (event) {
+    promoteMusic: function (event) {
         event.preventDefault();
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
-            return instance.packItem(App.upc, {from: App.metamaskAccountID});
+            return instance.promoteMusic(App.upc, {from: App.metamaskAccountID});
         }).then(function(result) {
             $("#ftc-item").text(result);
-            console.log('packItem',result);
+            console.log('promoteMusic',result);
         }).catch(function(err) {
             console.log(err.message);
         });
     },
 
-    sellItem: function (event) {
+    distributeMusic: function (event) {
+        event.preventDefault();
+        var processId = parseInt($(event.target).data('id'));
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            return instance.distributeMusic(App.upc, {from: App.metamaskAccountID});
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+            console.log('distributeMusic',result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    sellMusic: function (event) {
         event.preventDefault();
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
             const productPrice = web3.toWei(1000000, "gwei");
-            console.log('productPrice',productPrice);
-            return instance.sellItem(App.upc, App.productPrice, {from: App.metamaskAccountID});
+            console.log('musicPrice',musicPrice);
+            return instance.sellMusic(App.upc, App.musicPrice, {from: App.metamaskAccountID});
         }).then(function(result) {
             $("#ftc-item").text(result);
-            console.log('sellItem',result);
+            console.log('sellMusic',result);
         }).catch(function(err) {
             console.log(err.message);
         });
     },
 
-    buyItem: function (event) {
+    buyMusic: function (event) {
         event.preventDefault();
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
             const walletValue = web3.toWei(2000000, "gwei");
-            return instance.buyItem(App.upc, {from: App.metamaskAccountID, value: walletValue});
+            return instance.buyMusic(App.upc, {from: App.metamaskAccountID, value: walletValue});
         }).then(function(result) {
             $("#ftc-item").text(result);
-            console.log('buyItem',result);
+            console.log('buyMusic',result);
         }).catch(function(err) {
             console.log(err.message);
         });
     },
 
-    shipItem: function (event) {
+    listenToMusic: function (event) {
         event.preventDefault();
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
-            return instance.shipItem(App.upc, {from: App.metamaskAccountID});
+            return instance.listenToMusic(App.upc, {from: App.metamaskAccountID});
         }).then(function(result) {
             $("#ftc-item").text(result);
-            console.log('shipItem',result);
+            console.log('listenToMusic',result);
         }).catch(function(err) {
             console.log(err.message);
         });
     },
 
-    receiveItem: function (event) {
-        event.preventDefault();
-        var processId = parseInt($(event.target).data('id'));
-
-        App.contracts.SupplyChain.deployed().then(function(instance) {
-            return instance.receiveItem(App.upc, {from: App.metamaskAccountID});
-        }).then(function(result) {
-            $("#ftc-item").text(result);
-            console.log('receiveItem',result);
-        }).catch(function(err) {
-            console.log(err.message);
-        });
-    },
-
-    purchaseItem: function (event) {
-        event.preventDefault();
-        var processId = parseInt($(event.target).data('id'));
-
-        App.contracts.SupplyChain.deployed().then(function(instance) {
-            return instance.purchaseItem(App.upc, {from: App.metamaskAccountID});
-        }).then(function(result) {
-            $("#ftc-item").text(result);
-            console.log('purchaseItem',result);
-        }).catch(function(err) {
-            console.log(err.message);
-        });
-    },
-
-    fetchItemBufferOne: function () {
+    fetchmusicBufferOnePartOne: function () {
     ///   event.preventDefault();
     ///    var processId = parseInt($(event.target).data('id'));
         App.upc = $('#upc').val();
         console.log('upc',App.upc);
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
-          return instance.fetchItemBufferOne(App.upc);
+          return instance.fetchmusicBufferOnePartOne(App.upc);
         }).then(function(result) {
           $("#ftc-item").text(result);
-          console.log('fetchItemBufferOne', result);
+          console.log('fetchmusicBufferOnePartOne', result);
         }).catch(function(err) {
           console.log(err.message);
         });
     },
 
-    fetchItemBufferTwo: function () {
+    fetchmusicBufferOnePartTwo: function () {
     ///    event.preventDefault();
     ///    var processId = parseInt($(event.target).data('id'));
                         
         App.contracts.SupplyChain.deployed().then(function(instance) {
-          return instance.fetchItemBufferTwo.call(App.upc);
+          return instance.fetchmusicBufferOnePartTwo.call(App.upc);
         }).then(function(result) {
           $("#ftc-item").text(result);
-          console.log('fetchItemBufferTwo', result);
+          console.log('fetchmusicBufferOnePartTwo', result);
+        }).catch(function(err) {
+          console.log(err.message);
+        });
+    },
+
+    fetchmusicBufferOnePartThree: function () {
+    ///    event.preventDefault();
+    ///    var processId = parseInt($(event.target).data('id'));
+                        
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+          return instance.fetchmusicBufferOnePartThree.call(App.upc);
+        }).then(function(result) {
+          $("#ftc-item").text(result);
+          console.log('fetchmusicBufferOnePartThree', result);
         }).catch(function(err) {
           console.log(err.message);
         });
