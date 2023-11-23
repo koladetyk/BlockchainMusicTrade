10f6655ec5dd49da57790dae18e25f21b7779b95
ayokolade.erinle <47436776+koladetyk@users.noreply.github.com>
2023-11-23 16:42:12 +0100

    Update truffle.js

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const infuraKey = "a5a6be766c1245eda4c925fba44e8368";
-const mnemonic = "square book banner rigid during mix muscle olympic dilemma staff label brick";
+const mnemonic = "bread book banner toy during mix muscle sack dilemma staff label brick";
 
 module.exports = {
   networks: {
